594715ec3b9e6a47bfb6484f9cc62b4509fcc19e
Younes <44975764+yaz3@users.noreply.github.com>
2023-01-07 17:07:27 +0100

    adding upload video function

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func downloadVideo(videoURL string, apiKey string, startTime string, endTime str
 	defer resp.Body.Close()
 
 	// Create a file to save the video
-	file, err := os.Create("video.mp4")
+	file, err := os.Create(fmt.Sprintf("%s.mp4",videoID))
 	if err != nil {
 		return fmt.Errorf("error creating file: %v", err)
 	}
@@ -88,7 +88,66 @@ func downloadVideo(videoURL string, apiKey string, startTime string, endTime str
 	return nil
 }
 
-err := downloadVideo("VIDEO_ID", "YOUR_API_KEY", "00:00:10", "00:00:20")
-if err != nil {
-	// handle error
+func uploadVideo(filePath string) (*youtube.Video, error) {
+	ctx := context.Background()
+
+	// Set up the YouTube Data API client
+	client, err := youtube.NewService(ctx, option.WithAPIKey("YOUR_API_KEY"))
+	if err != nil {
+		return nil, fmt.Errorf("Error creating YouTube client: %v", err)
+	}
+
+	// Read the file that you want to upload
+	videoBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading file: %v", err)
+	}
+
+	// Set up the metadata for the video
+	video := &youtube.Video{
+		Snippet: &youtube.VideoSnippet{
+			Title:       "My Video",
+			Description: "This is my video",
+			CategoryId:  "22",
+		},
+		Status: &youtube.VideoStatus{
+			PrivacyStatus: "private",
+		},
+	}
+
+	// Create a request to insert the video
+	insertCall := client.Videos.Insert("snippet,status", video)
+
+	// Set the file to be uploaded
+	media := &youtube.Media{
+		Body: bytes.NewReader(videoBytes),
+	}
+	insertCall.Media(media)
+
+	// Execute the request and return the response
+	response, err := insertCall.Do(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Error executing request: %v", err)
+	}
+	return response, nil
 }
+
+func main() {
+	videoURL := "URL"
+	videoID := "ID" //TODO: parseID from url (v=) and get ride of the parsing in the download video, use id instead
+	apiKey := ="KEY"
+	startTime := "00:00:10"
+	endTime := "00:00:20"
+	err := downloadVideo(videoURL, apiKey, startTime, endTime)
+	if err != nil {
+		// handle error
+	}
+
+	// Upload a video
+	video, err := uploadVideo(fmt.Sprintf("%s.mp4",videoID)) //TODO: pass the metadata and media info as a param struct
+	if err != nil {
+		log.Fatalf("Error uploading video: %v", err)
+	}
+	fmt.Printf("Uploaded video: %v\n", video.Id)
+}
+
